888814127a66cbe29937e52e2df625a26d145f52
amos <amosstaileyyoung@gmail.com>
2025-06-25 13:58:05 -0400

    fixed so that docker container can be killed when imported via package

--- cineface/pipeline.py
+++ cineface/pipeline.py
@@ -3,12 +3,18 @@ import os
 os.environ["TF_USE_LEGACY_KERAS"] = "1"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+import gc
+import sys
+import signal
 import logging
+import subprocess as sp
 import multiprocessing as mp
 from pathlib import Path
 from argparse import ArgumentParser
 
+import docker
 import pandas as pd
+from tensorflow.keras import backend as K
 from qdrant_client import QdrantClient 
 
 from cineface.metadata import get_metadata
@@ -41,70 +47,66 @@ def add_metadata(df, metadata):
     return df
 
 
-def match_faces_worker(src, client_info, encoding_col, recognition_model, threshold, batch_size, timeout):
-    import gc
-    from tensorflow.keras import backend as K
+class Pipeline(object):
+    def __init__(self, client_info):
+        host, port = client_info
+        self.client = QdrantClient(host=host, port=port)
+        logger.debug(f'Successfully connected to Qdrant database at {host}: {port}')
 
-    df = pd.read_csv(src)
-    qdrant_client, qdrant_port = client_info
-    client = QdrantClient(host=qdrant_client, port=qdrant_port)
-    logger.debug(f'Successfully connected to Qdrant database at {qdrant_client}: {qdrant_port}')
+        self.docker_client = docker.from_env()
+        self.container = None
 
-    result_df = match_faces(df, 
-                            client, 
-                            encoding_col=encoding_col,
-                            recognition_model=recognition_model,
-                            threshold=threshold,
-                            batch_size=batch_size,
-                            timeout=timeout)
-    # result_df.to_csv('temp_matched.csv', index=False)
-    K.clear_session()
-    gc.collect()
-    return result_df
+    def handle_sigint(self, sig, frame):
+        print("\nCtrl+C detected! Attempting to stop Docker container...")
+        if self.container_name:
+            try:
+                sp.run(['docker', 'stop', '-t', '1', self.container_name], stdout=sp.DEVNULL, stderr=sp.DEVNULL, timeout=30)
+                print(f"Container '{self.container_name}' stopped.")
+            except Exception as e:
+                print(f"Failed to stop container: {e}")
+        else:
+            print("No container to stop.")
+        sys.exit(1)
 
+    def run_pipeline(self,
+                     file,
+                     frameskip=24,
+                     encoding_col='encoding',
+                     image='astaileyyoung/visage',
+                     model_dir=Path.home() / '.visage/models',
+                     log_level='info',
+                     show=False,
+                     recognition_model='Facenet',
+                     threshold=0.5,
+                     timeout=60,
+                     batch_size=256,
+                     metadata=None):
+        if not metadata:
+            metadata = get_metadata(file)
+        
+        data, self.container_name = run_visage(file, 'temp.csv', image, frameskip, log_level, show, model_dir)
+        if not data:
+            return 
+        
+        df = pd.DataFrame(data)
+        
+        df = add_metadata(df, metadata)
+        logger.info('Matching faces ...')
+        
+        df = match_faces(df, 
+                         self.client, 
+                         encoding_col=encoding_col,
+                         recognition_model=recognition_model,
+                         threshold=threshold,
+                         batch_size=batch_size,
+                         timeout=timeout)
+        logger.info('Finished matching.')
 
-def pipeline(file,
-             client_info,
-             frameskip=24,
-             encoding_col='encoding',
-             image='astaileyyoung/visage',
-             model_dir=Path.home() / '.visage/models',
-             log_level='info',
-             show=False,
-             recognition_model='Facenet',
-             threshold=0.5,
-             timeout=60,
-             batch_size=256,
-             metadata=None):
-    if not metadata:
-        metadata = get_metadata(file)
-    
-    run_visage(file, 'temp.csv', image, frameskip, log_level, show, model_dir)
-    df = pd.read_csv('temp.csv')
-    
-    df = add_metadata(df, metadata)
-    logger.info('Matching faces ...')
-    df.to_csv('temp_meta.csv', index=False)
+        K.clear_session()
+        gc.collect()
 
-    with mp.get_context('spawn').Pool(1) as pool:
-        async_result = pool.apply_async(
-            match_faces_worker,
-            ('temp_meta.csv', client_info, encoding_col, recognition_model, threshold, batch_size, timeout)
-        )
-        df = async_result.get()
-    logger.info('Finished matching.')
-    
-    # df = pd.read_csv('temp_matched.csv')
-    # df = match_faces(df, 
-    #                  client,
-    #                  encoding_col=encoding_col,
-    #                  recognition_model=recognition_model,
-    #                  threshold=threshold,
-    #                  batch_size=batch_size,
-    #                  timeout=timeout)
-    Path('temp.csv').unlink()
-    Path('temp_meta.csv').unlink()
-    return df
+        return df
+
 
 
 def main():
@@ -149,19 +151,21 @@ def main():
 
     logger.info(f'Running detection on {args.src}')
     logger.info(f'Saving results to {args.dst}')
-    df = pipeline(
-            args.src, 
-            (args.qdrant_client, args.qdrant_port),
-            encoding_col=args.encoding_col,
-            image=args.image,
-            frameskip=args.frameskip,
-            log_level=args.log_level,
-            show=args.show,
-            threshold=args.threshold,
-            timeout=args.timeout,
-            batch_size=args.batch_size,
-            metadata=metadata)
-
+    pipe = Pipeline((args.qdrant_client, args.qdrant_port))
+    df = pipe.run_pipeline(args.src, 
+                           encoding_col=args.encoding_col,
+                           image=args.image,
+                           frameskip=args.frameskip,
+                           log_level=args.log_level,
+                           show=args.show,
+                           threshold=args.threshold,
+                           timeout=args.timeout,
+                           batch_size=args.batch_size,
+                           metadata=metadata
+    )
+    if df is None:
+        exit() 
+    
     if args.faces_dir:
         save_faces(args.dst, args.faces_dir, label='predicted_name')
 
